0b90802155e3b0694976d5a91322dd55424f7974
VidDurejaArchive <vdurej2@illinois.edu>
2025-06-30 21:33:03 -0500

    Update import pandas as pd.py

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -1,7 +1,1 @@
-import pandas as pd
 
-df1 = pd.read_csv("samsung_sentiment.csv")
-df2 = pd.read_csv("samsung_stock.csv")
-
-print("📄 Sentiment CSV Columns:", df1.columns.tolist())
-print("📄 Stock CSV Columns:", df2.columns.tolist())
